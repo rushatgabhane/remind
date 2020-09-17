2167754cfd6857055e76156638bfa207a2c6d620
rushatgabhane <rushatgabhane@gmail.com>
2020-09-17 14:07:30 +0530

    fix function args

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     description: 'Ping!',
     args: false,
-    execute(message, args){
+    execute(message, args, pgClient){
         return message.channel.send('pong 🏓');
     }
 };
--- commands/remind.js
+++ commands/remind.js
@@ -3,9 +3,9 @@ module.exports = {
     description: 'Remind to game every interval',
     usage: '!remind []',
     args: true,
-    execute(message, args){
+    execute(message, args, pgClient){
         users = ["726865904595632179", "260390293881356294", "361140440361205772", "617322295685283875"];
-        
+
         data.push
         message.channel.send(data, { split: true });
     }
