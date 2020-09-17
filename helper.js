11a789cac5d2063793bbf11c29f39236bc6f292a
rushatgabhane <rushatgabhane@gmail.com>
2020-09-18 02:04:31 +0530

    fix typeerror

--- helper.js
+++ helper.js
@@ -1,25 +1,20 @@
-const getNameFromId = (id) => {
-    return 'Jim';
-};
-  
-const remind = (message, users, data) => {
-    for(i=0; i<users.length; i++){
-        const User = Client.fetchUser(users[i]);
-        console.log(User);
-        data.push(`@${User.username}${User.discriminator} `);
+module.exports = {
+    getNameFromId (id){
+        return 'Jim';
+    },
+    remind(message, users, data){
+        for(i=0; i<users.length; i++){
+            const User = Client.fetchUser(users[i]);
+            console.log(User);
+            data.push(`@${User.username}${User.discriminator} `);
+        }
+        return message.channel.send(data);
+    },
+    isValidHourOrMinute(minOrHour){
+        if(parseInt(minOrHour) >= 0 &&
+        parseInt(minOrHour) <= 60){
+            return true;
+        }
+        return false;
     }
-    return message.channel.send(data);
-};
-
-const isValidHourOrMinute = (minOrHour) => {
-    if(parseInt(minOrHour) >= 0 &&
-    parseInt(minOrHour) <= 60){
-        return true;
-    }
-    return false;
 }
-
-  
-exports.getName = this.getNameFromId;
-exports.message = this.message;
-exports.isValidHourOrMinute = this.isValidHourOrMinute;
