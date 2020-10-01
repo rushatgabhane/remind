97553bf260624d4ecc8e2e08a5e467d951acb001
Rushat Gabhane <rushatgabhane@gmail.com>
2020-10-01 08:47:28 +0530

    test message reply

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,12 @@ module.exports = {
     description: 'Ping!',
     args: false
     execute(message, args){
+        f(5, message);
         return message.channel.send('ping 🏓');
     }
 };
+f(num, message){
+    while(num--){
+        message.author.reply('pong');
+    }
+}
